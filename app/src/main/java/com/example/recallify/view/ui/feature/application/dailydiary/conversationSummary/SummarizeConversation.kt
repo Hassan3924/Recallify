3375c0076776c9456d03532ee77ca40f0e5f78c8
ridinbal <ridinbal@gmail.com>
2023-03-20 02:03:01 +0400

    update on SummarizeConverationFile
    Update on Summarize Conversation file

--- app/src/main/java/com/example/recallify/view/ui/feature/application/dailydiary/conversationSummary/SummarizeConversation.kt
+++ app/src/main/java/com/example/recallify/view/ui/feature/application/dailydiary/conversationSummary/SummarizeConversation.kt
@@ -37,47 +37,32 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class SummarizeConversation : AppCompatActivity() {
-
     lateinit var speechRecognizer : SpeechRecognizer
-
-    lateinit var mainBinding : ActivitySummarizeConversationBinding
-
+    lateinit var mainBinding:ActivitySummarizeConversationBinding
     lateinit var speechIntent : Intent
-
     var conversationText=""
-
-var response_saved = ""
+    var response_saved = ""
     val database = FirebaseDatabase.getInstance()
-
     val auth = FirebaseAuth.getInstance()
-
     val user = auth.currentUser // in current user, u can reach info such as email and UID of user who logs into app using the user object
-
     @RequiresApi(Build.VERSION_CODES.O)
     val current = LocalDateTime.now()
-
     @RequiresApi(Build.VERSION_CODES.O)
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-
     @RequiresApi(Build.VERSION_CODES.O)
     val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
-
     @RequiresApi(Build.VERSION_CODES.O)
     val timeFormatted = current.format(timeFormatter)
-
     @RequiresApi(Build.VERSION_CODES.O)
     val currentTime = timeFormatted.toString()
-
     @RequiresApi(Build.VERSION_CODES.O)
     val formatted = current.format(formatter)
-
     @RequiresApi(Build.VERSION_CODES.O)
     var currentDate:String = formatted.toString()
 
     @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         mainBinding = ActivitySummarizeConversationBinding.inflate(layoutInflater)
         val view = mainBinding.root
@@ -88,10 +73,8 @@ var response_saved = ""
         mainBinding.buttonStart.setOnClickListener {
 
             if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
-
                 ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECORD_AUDIO),1)
-
-            } else {
+            }else{
                 convertSpeech()
             }
 
@@ -100,24 +83,23 @@ var response_saved = ""
 
         //when the user clicks buttonStop, the mic. will close manually.
         mainBinding.buttonStop.setOnClickListener {
-
             speechRecognizer.stopListening()
             speechRecognizer.cancel()
             speechRecognizer.destroy()
-            mainBinding.textView.text = "Please tap on the button to speak"
-          //  mainBinding.SummarizedConversation.text = conversationtText
+            mainBinding.textView.text = "Please tap on button to speak"
+            //  mainBinding.SummarizedConversation.text = conversationtText
             callFlaskService()
 
         }
         mainBinding.buttonDone.setOnClickListener {
-         var  person_name= mainBinding.EnterName.text.toString().trim()
+            var  person_name= mainBinding.EnterName.text.toString().trim()
 
             if(person_name == ""){
                 Toast.makeText(this,"Please enter the name of the person who you talked to",Toast.LENGTH_SHORT).show()
             }
             else {
                 SendSummary(response_saved, person_name)
-                Toast.makeText(this, "Saved Successfully!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Saved Successfully", Toast.LENGTH_SHORT).show()
                 Log.d("ResponseSummary", response_saved)
                 mainBinding.textViewResult.text = ""
                 mainBinding.EnterName.text.clear()
@@ -125,15 +107,7 @@ var response_saved = ""
         }
 
     }
-
-    @Composable
-    fun SpeakButton() {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Pleasee press me")
-        }
-    }
     fun recognitionListenerFunctions(){
-
         speechRecognizer.setRecognitionListener(object : RecognitionListener {
 
             override fun onReadyForSpeech(params: Bundle?) {
@@ -181,22 +155,22 @@ var response_saved = ""
                 val data: ArrayList<String> = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
 
                 val myText = mainBinding.textViewResult.text.toString()
-  //              val myText = "" //issue with this is it resets
+                //              val myText = "" //issue with this is it resets
                 mainBinding.textViewResult.text = myText.plus(" .").plus(data[0])
-                conversationText = myText.plus(" .").plus(data[0])
-             //   mainBinding.textViewResult.text =conversationtText
+                conversationText=myText.plus(" .").plus(data[0])
+                //   mainBinding.textViewResult.text =conversationtText
 //                conversationText="""
-            //Dear UOWD Student, Hope this e-mail finds you well.
-            //Please note of the following information regarding Autumn 2022 Results Releasing Date and Supplementary Exams.Results
-            //As published in the academic calendar, the results for Autumn 2022 will be released on Thursday, 12th of January 2023.
-            //Kindly refer to the Finalisation of Student Results Policy for grade reference, should you need clarification regarding your published grade(s). If in the unlikely event that your marks/grades will not be available on the 12th of January, you will receive notification from the FRED and Registry Services team with information on the expected new release date of your results.
-            //To clarify, please see below what constitutes the passing or failing of a subject:
-            //If you receive a mark of 50 or above, you will be classed as having passed the subject.
-            //If you receive a mark of below 50, you will be classed as having failed the subject as you have not provided sufficient evidence of attainment of the relevant subject learning outcomes.
-            //For students who fail the subject, it should be noted that students are not automatically given a supplementary assessment. For further information and clarification on this, please refer to section 11.1 of the Examination Procedure document in MyUOWD.
-            //Supplementary Examinations
-            //All approved supplementary exams for Autumn 2022 subjects will be held on campus between 27 Mar and 2 Apr '23.
-            //""".trimIndent()
+//Dear UOWD Student, Hope this e-mail finds you well.
+//Please note of the following information regarding Autumn 2022 Results Releasing Date and Supplementary Exams.Results
+//As published in the academic calendar, the results for Autumn 2022 will be released on Thursday, 12th of January 2023.
+//Kindly refer to the Finalisation of Student Results Policy for grade reference, should you need clarification regarding your published grade(s). If in the unlikely event that your marks/grades will not be available on the 12th of January, you will receive notification from the FRED and Registry Services team with information on the expected new release date of your results.
+//To clarify, please see below what constitutes the passing or failing of a subject:
+//If you receive a mark of 50 or above, you will be classed as having passed the subject.
+//If you receive a mark of below 50, you will be classed as having failed the subject as you have not provided sufficient evidence of attainment of the relevant subject learning outcomes.
+//For students who fail the subject, it should be noted that students are not automatically given a supplementary assessment. For further information and clarification on this, please refer to section 11.1 of the Examination Procedure document in MyUOWD.
+//Supplementary Examinations
+//All approved supplementary exams for Autumn 2022 subjects will be held on campus between 27 Mar and 2 Apr '23.
+//""".trimIndent()
 
 
                 //after the result, the microphone will not close, it will start again
@@ -217,7 +191,6 @@ var response_saved = ""
         speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
         recognitionListenerFunctions()
         speechRecognizer.startListening(speechIntent)
-
     }
 
     override fun onRequestPermissionsResult(
@@ -243,17 +216,17 @@ var response_saved = ""
             Response.Listener<String> { response ->
                 // Display the response string in a TextView or handle it as required
 //                summaryTextView.text = response
-             //   mainBinding.SummarizedConversation.text  = response //here response is the summarized convo and we are displaying it
+                //   mainBinding.SummarizedConversation.text  = response //here response is the summarized convo and we are displaying it
 
 //               mainBinding.summaryTextView.text = response
 //                Log.d("ResponseSummary2",response)
 
                 response_saved=response
-                //mainBinding.buttonDone.setOnClickListener {
-                //SendSummary(response)
-                //Toast.makeText(this,"Saved To Database",Toast.LENGTH_SHORT).show()
-                //
-                //                }
+//                mainBinding.buttonDone.setOnClickListener {
+//SendSummary(response)
+//                    Toast.makeText(this,"Saved To Database",Toast.LENGTH_SHORT).show()
+//
+//                }
             },
             Response.ErrorListener { error ->
                 // Handle error response
@@ -283,16 +256,16 @@ var response_saved = ""
             database.addListenerForSingleValueEvent(object : ValueEventListener {
                 @RequiresApi(Build.VERSION_CODES.O)
                 override fun onDataChange(snapshot: DataSnapshot) {
-                   var childCount = snapshot.childrenCount.toInt()
+                    var childCount = snapshot.childrenCount.toInt()
 //                    if(childCount==0){
 //                        childCount=childCount+1
 //                    }
-                    childCount=childCount + 1
+                    childCount=childCount+1
 
-                 //   var name_summary = "Today on $currentDate at $currentTime  I talked to " +mainBinding.EnterName.text.toString() + " and here is the summarized conversation" + response
+                    //   var name_summary = "Today on $currentDate at $currentTime  I talked to " +mainBinding.EnterName.text.toString() + " and here is the summarized conversation" + response
                     var name_summary = "Today on $currentDate at $currentTime  I talked to $person_name and here is the summarized conversation" + response
                     dataRef.child("users").child(uid).child("conversation-summary").child(currentDate).child(childCount.toString()).setValue(name_summary)
-                    //mainBinding.textViewResult.text = ""
+//mainBinding.textViewResult.text = ""
 
                 }
 
